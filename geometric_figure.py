0ae72fee8db19580153a347c633e108e06ae54d3
EvgenIvanov <evgen2007@bk.ru>
2020-12-02 21:25:57 +0300

    add description

--- geometric_figure.py
+++ geometric_figure.py
@@ -1,3 +1,6 @@
+## Unit B
+#  task 6.10.1
+#
 class Rectangle:
     def __init__(self, x, y, width, height):
         self.x = x
@@ -8,5 +11,4 @@ class Rectangle:
         return Rectangle.__name__ + str ((self.x, self.y, self.width, self.height))
 
 figure = Rectangle(5, 10, 50, 100)
-
 print(figure.get_settings())
