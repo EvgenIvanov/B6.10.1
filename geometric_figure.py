aeb985042b3177cd2b826536ba28e75b618ffaf3
EvgenIvanov <evgen2007@bk.ru>
2020-12-02 21:22:32 +0300

    init

---
+++ geometric_figure.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def get_settings(self):
+        return Rectangle.__name__ + str ((self.x, self.y, self.width, self.height))
+
+figure = Rectangle(5, 10, 50, 100)
+
+print(figure.get_settings())
